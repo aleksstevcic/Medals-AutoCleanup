c0f1ccd1a730e86b75616724de275513f4c5212d
Aleksandar Stevcic <aleksandarstevcic.as@gmail.com>
2022-05-30 01:28:03 -0400

    added mkv

--- scripts/MedalsRM.bat
+++ scripts/MedalsRM.bat
@@ -7,6 +7,7 @@ set pathtoJSON=""
 set /A timetodelete=7
 
 ForFiles /p %pathtoclips% /s /d -%timetodelete% /m *.mp4 /c "cmd /c del @path"
+ForFiles /p %pathtoclips% /s /d -%timetodelete% /m *.mkv /c "cmd /c del @path"
 ForFiles /p %pathtoclips% /s /d -%timetodelete% /m *.jpg /c "cmd /c del @path"
 
 python %pathtoscripts%\cleanJSON.py %pathtoJSON%
